17d8dc8821e8c330d30f0daf3cbb4ab4d99c9cec
Polin <polina996@mail.ru>
2023-06-03 21:21:40 +0300

    main

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ public class Main {
     public static void main (String[] args) {
         store store = new store();
 
-        // add some toys
         store.addToy(1, "Toy 1", 10, 20);
         store.addToy(2, "Toy 2", 5, 10);
         store.addToy(3, "Toy 3", 20, 70);
